23d7b50036fbd2d475ab36aec621f3a619c8d8a7
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-04-06 15:32:07 +0100

    :snake: increase flexibility of processing so if the parameter isn't there it will still return a dataset

--- R/utils_tfmt.R
+++ R/utils_tfmt.R
@@ -3,6 +3,27 @@ apply_tfmt <- function(.data, tfmt){
   apply_all_fmts(.data, tfmt$body_style, group = tfmt$group, label = tfmt$label, param = tfmt$param, tfmt$values) %>%
     pivot_wider(names_from = !!tfmt$column,
                 values_from = !!tfmt$values) %>%
-    arrange(!!!tfmt$sorting_cols) %>%
-    select(!!!tfmt$col_select)
+    tentative_process(arrange, tfmt$sorting_cols) %>%
+    tentative_process(arrange, tfmt$col_select)
+}
+
+
+#' Tentatively apply an element
+#'
+#' Will only apply the element to the data if the param isn't null
+#'
+#' @param .data data to process
+#' @param fx processing function
+#' @param param parameter to control processing
+#'
+#' @return processed data
+#' @noRd
+tentative_process <- function(.data, fx, param){
+  if(is.null(param)){
+    out <- .data
+  } else {
+    out <- .data %>%
+      fx(!!!param)
+  }
+  out
 }
