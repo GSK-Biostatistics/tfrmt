6376007fce3b56008a2d2ef1c55d07a6ab48e75e
Christina Fillmore <christina.e.fillmore@gsk.com>
2022-04-07 11:27:22 +0100

    Add groupings

--- R/print.R
+++ R/print.R
@@ -4,7 +4,9 @@ print_mock_gt <- function(tfmt) {
 
 print_to_gt <- function(tfmt, .data){
   apply_tfmt(.data, tfmt) %>%
-    gt() %>%
+    gt(
+      groupname_col = as_label(tfmt$group[[1]]),
+      rowname_col = as_label(tfmt$label)) %>%
     tab_header(title = tfmt$title,
                subtitle = tfmt$subtitle) %>%
     apply_gt_footnote(tfmt$footer)
