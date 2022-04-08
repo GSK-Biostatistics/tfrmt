d52a2b1d698b0b4c67798402b3afa494bb8904a6
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-04-08 11:24:29 -0700

    remove "preferred" from layer_tfmt, update tests accordingly

--- R/tfmt_layer.R
+++ R/tfmt_layer.R
@@ -1,11 +1,11 @@
 #' Layer tfmt objects together
 #'
 #' Provide utility for layering tfmt objects together. If both tmft's have
-#' values, it will preferentially choose the second (y) tfmt by default.
+#' values, it will preferentially choose the second tfmt by default.
 #'
 #' @param x,y tfmt objects that need to be combined
 #' @param ... arguments passed to layer_tfmt_arg functions for combining different tfmt elements
-#' @param preferred which tfmt should be used when layering and both have values. "x" or "y", defaults to "y".
+#' @param join_body_styles should the body styles be uniquely combined, or just keep styling in y
 #'
 #' @examples
 #'
@@ -15,7 +15,7 @@
 #'
 #' layered_table_format <- layer_tfmt(tfmt_1, tfmt_2)
 #'
-layer_tfmt <- function(x, y, ..., preferred = "y"){
+layer_tfmt <- function(x, y, ..., join_body_styles = TRUE){
 
   stopifnot(is_tfmt(x))
   stopifnot(is_tfmt(y))
@@ -26,14 +26,12 @@ layer_tfmt <- function(x, y, ..., preferred = "y"){
     return(x)
   }
 
-  preferred <- match.arg(preferred, c("y","x"))
-
   args <- union(names(x), names(y))
 
-  arg_list <- lapply(args, function(argname, x, y, ..., preferred){
+  arg_list <- lapply(args, function(argname, x, y, ..., join_body_styles){
     func <- get_layer_tfmt_arg_method(argname)
-    func(x, y, argname, ..., preferred = preferred)
-  },x=x, y = y, ..., preferred = preferred)
+    func(x, y, argname, ..., join_body_styles = join_body_styles)
+  },x=x, y = y, ..., join_body_styles = join_body_styles)
 
   names(arg_list) <- args
 
@@ -47,41 +45,25 @@ get_layer_tfmt_arg_method <- function(argname){
   )
 }
 
-layer_tfmt_arg.default<- function(x, y, arg_name, ..., preferred = "y"){
+layer_tfmt_arg.default<- function(x, y, arg_name, ...){
   x_arg_val <- x[[arg_name]]
   y_arg_val <- y[[arg_name]]
 
-  if(preferred == "y"){
-    preferred_arg_val <- y_arg_val
-    alt_arg_val <- x_arg_val
+  if(is.null(y_arg_val)){
+    x_arg_val
   }else{
-    preferred_arg_val <- x_arg_val
-    alt_arg_val <- y_arg_val
-  }
-
-  if(is.null(preferred_arg_val)){
-    alt_arg_val
-  }else{
-    preferred_arg_val
+    y_arg_val
   }
 }
 
-layer_tfmt_arg.body_style <- function(x, y, ..., preferred = "y", override_body_style = FALSE){
+layer_tfmt_arg.body_style <- function(x, y, ...,  join_body_styles = TRUE){
   x_body_style <- x[["body_style"]][["all_fmts"]]
   y_body_style <- y[["body_style"]][["all_fmts"]]
 
-  if(preferred == "y"){
-    preferred_arg_val <- y_body_style
-    alt_arg_val <- x_body_style
+  if(join_body_styles){
+    body_style_el <- unique(c(x_body_style, y_body_style))
   }else{
-    preferred_arg_val <- x_body_style
-    alt_arg_val <- y_body_style
-  }
-
-  if(override_body_style){
-    body_style_el <- preferred_arg_val
-  }else{
-    body_style_el <- unique(c(alt_arg_val, preferred_arg_val))
+    body_style_el <- y_body_style
   }
 
   do.call(element_style,body_style_el)
--- tests/testthat/test-tfmt.R
+++ tests/testthat/test-tfmt.R
@@ -57,8 +57,9 @@ test_that("basic tfmt - selecting group/label/param/values/column - char", {
   expect_equal( t_fmt$column, quo(column), ignore_attr = TRUE)
 })
 
-
 test_that("basic tfmt - selecting group/label/param/values/column - bare", {
+
+  skip("Not functional yet")
 
   t_fmt <- tfmt(
     title = "Table Title",
@@ -120,29 +121,17 @@ test_that("layering tfmt - select latest table elements",{
     title = "Table Title"
   )
 
-  t_fmt_layered_y <- t_fmt_title %>%
+  t_fmt_layered <- t_fmt_title %>%
     layer_tfmt(
       tfmt(
         title = "Table Title 2",
         subtitle = "Table Subtitle"
     ))
 
-  t_fmt_layered_x <- t_fmt_title %>%
-    layer_tfmt(
-      tfmt(
-        title = "Table Title 2",
-        subtitle = "Table Subtitle"
-      ),
-      preferred = "x")
 
-  expect_s3_class( t_fmt_layered_y,"tfmt"  )
-  expect_s3_class( t_fmt_layered_x,"tfmt"  )
-
-  expect_equal( t_fmt_layered_y$title, "Table Title 2")
-  expect_equal( t_fmt_layered_x$title, "Table Title")
-
-  expect_equal( t_fmt_layered_y$subtitle, "Table Subtitle")
-  expect_equal( t_fmt_layered_x$subtitle, "Table Subtitle")
+  expect_s3_class( t_fmt_layered,"tfmt"  )
+  expect_equal( t_fmt_layered$title, "Table Title 2")
+  expect_equal( t_fmt_layered$subtitle, "Table Subtitle")
 
 })
 
@@ -159,7 +148,7 @@ test_that("layering tfmt - body style elements",{
     )
 
 
-  t_fmt_layered_y <- t_fmt_title %>%
+  t_fmt_layered <- t_fmt_title %>%
     layer_tfmt(
       tfmt(
         body_style = element_style(
@@ -171,36 +160,14 @@ test_that("layering tfmt - body style elements",{
       )
     )
 
-  t_fmt_layered_x <- t_fmt_title %>%
-    layer_tfmt(
-      tfmt(
-        body_style = element_style(
-          fmt_str(
-            group = c("group2"),
-            fmt("xx.x")
-          )
-        )
-      ),
-      preferred = "x"
-    )
+  expect_s3_class(t_fmt_layered, "tfmt")
 
-  expect_s3_class(t_fmt_layered_y, "tfmt")
-  expect_s3_class(t_fmt_layered_x, "tfmt")
-
-  expect_equal( t_fmt_layered_y$title, "Table Title")
-  expect_equal( t_fmt_layered_x$title, "Table Title")
-
-  expect_equal(t_fmt_layered_y$body_style,
+  expect_equal( t_fmt_layered$title, "Table Title")
+  expect_equal(t_fmt_layered$body_style,
                element_style(fmt_str(group = "group1",
                                      fmt(rounding = "XXX")),
                              fmt_str(group = "group2",
                                      fmt("xx.x"))))
-
-  expect_equal(t_fmt_layered_x$body_style,
-               element_style(fmt_str(group = "group2",
-                                     fmt("xx.x")),
-                             fmt_str(group = "group1",
-                                     fmt(rounding = "XXX"))))
 
 })
 
@@ -243,7 +210,7 @@ test_that("layering tfmt - body style elements - multiple",{
 
 })
 
-test_that("layering tfmt - body style elements - override_body_style",{
+test_that("layering tfmt - body style elements - join_body_style FALSE",{
 
   t_fmt_title <- tfmt(
     title = "Table Title",
@@ -255,7 +222,7 @@ test_that("layering tfmt - body style elements - override_body_style",{
     )
   )
 
-  t_fmt_layered_y <- t_fmt_title %>%
+  t_fmt_layered <- t_fmt_title %>%
     layer_tfmt(
       tfmt(subtitle = "Table Subtitle",
            body_style = element_style(
@@ -263,42 +230,21 @@ test_that("layering tfmt - body style elements - override_body_style",{
              fmt_str(group_val = "group3",fmt("xx.xx"))
            )
       ),
-      override_body_style = TRUE
+      join_body_styles = FALSE
     )
 
-  t_fmt_layered_x <- t_fmt_title %>%
-    layer_tfmt(
-      tfmt(subtitle = "Table Subtitle",
-           body_style = element_style(
-             fmt_str(group_val = "group2",fmt("xx.x")),
-             fmt_str(group_val = "group3",fmt("xx.xx"))
-           )
-      ),
-      preferred = "x",
-      override_body_style = TRUE
-    )
 
-  expect_s3_class(t_fmt_layered_y,"tfmt")
-  expect_s3_class(t_fmt_layered_x,"tfmt")
+  expect_s3_class(t_fmt_layered,"tfmt")
 
 
-  expect_equal(t_fmt_layered_y$title, "Table Title")
-  expect_equal(t_fmt_layered_x$title, "Table Title")
-  expect_equal(t_fmt_layered_y$subtitle, "Table Subtitle")
-  expect_equal(t_fmt_layered_x$subtitle, "Table Subtitle")
+  expect_equal(t_fmt_layered$title, "Table Title")
+  expect_equal(t_fmt_layered$subtitle, "Table Subtitle")
 
   expect_equal(
-    t_fmt_layered_y$body_style,
+    t_fmt_layered$body_style,
     element_style(
       fmt_str(group_val = "group2",fmt("xx.x")),
       fmt_str(group_val = "group3",fmt("xx.xx"))
-    )
-  )
-
-  expect_equal(
-    t_fmt_layered_x$body_style,
-    element_style(
-      fmt_str(group_val = "group1",fmt("XXX"))
     )
   )
 
