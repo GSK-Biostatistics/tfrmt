f29a45f685590b262b8345d77757960ef6888edd
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-04-19 11:27:44 -0700

    Add tests, update documentation to reflect imports

--- R/apply_tfrmt.R
+++ R/apply_tfrmt.R
@@ -1,141 +1,20 @@
 
-#' #' @import dplyr
-#' #'
-#' #'
-#' NULL
-#'
-#' #' Apply the formatting to all values in the dataset
-#' #'
-#' #' @param .data data
-#' #' @param element_style styling element needed
-#' #' @param group symbolic list of grouping
-#' #' @param label symbolic label
-#' #' @param param symbolic parameter
-#' #' @param values symbolic value
-#' #'
-#' #' @noRd
-#' #' @importFrom dplyr tibble mutate group_by arrange slice bind_cols group_split pull select starts_with
-#' #' @importFrom purrr map map_dfr
-#' #' @importFrom tidyr unnest
-#' #' @importFrom rlang !!
-#' apply_table_frmt_plan <- function(.data, table_frmt_plan, group, label, param, values, column){
-#'
-#'   .data$`..frmt_values` <- ""
-#'   .data$`..applied_frmt_values` <- FALSE
-#'   browser()
-#'
-#'   for(frmt_struct in rev(table_frmt_plan)){
-#'
-#'     .data <- apply_frmt_structure(
-#'        struct = frmt_struct,
-#'        .data = .data,
-#'        group = group,
-#'        label = label,
-#'        param = param,
-#'        values = values,
-#'        column = column,
-#'        frmt_values = "..frmt_values",
-#'        applied_frmt_values = "..applied_frmt_values"
-#'     )
-#'
-#'   }
+#' @import dplyr
 #'
 #'
-#'
-#'   .data %>%
-#'     mutate(
-#'       !!values := case_when(
-#'         `..applied_frmt_values` == TRUE ~ `..frmt_values`,
-#'         TRUE ~ as.character(!!values)
-#'       )
-#'     ) %>%
-#'     select(-`..applied_frmt_values`, - `..frmt_values`, -!!param)
-#'
-#' }
-#'
-#'
-#'
-#' #' Apply format structure to data
-#' #'
-#' #'
-#' #' @param struct format structure
-#' #' @param .data data to apply format structure to
-#' #' @param group list of the group parameters
-#' #' @param label label symbol should only be one
-#' #' @param column column symbol should only be one
-#' #' @param param param symbol should only be one
-#' #' @param values values symbol should only be one
-#' #' @param frmt_values column name to write applied format to
-#' #' @param applied_frmt_values column name to document that format has been applied
-#' #'
-#' #' @return
-#' #' @noRd
-#' #' @importFrom stringr str_count str_trim str_dup str_c str_remove
-#' #' @importFrom dplyr if_else case_when tibble
-#' #' @importFrom purrr map_lgl
-#'
-#' apply_frmt_structure <- function(struct, .data, group, label, column, param, values, frmt_values, applied_frmt_values){
-#'   struct_locs <- idx_frmt_group_lab_rows(
-#'     struct_to_apply = struct,
-#'     .data = .data,
-#'     group = group,
-#'     label = label,
-#'     param = param
-#'     )
-#'
-#'   .data_sub <- .data[struct_locs,]
-#'
-#'   struct_frmts <- unique(tibble(param = struct$param, frmt_to_apply = struct$frmt_to_apply))
-#'   ## set default to always be last
-#'
-#'   if(".default" %in% struct_frmts$param & nrow(struct_frmts) > 1){
-#'     struct_frmts <- bind_rows(
-#'       struct_frmts[struct_frmts$param != ".default",],
-#'       struct_frmts[struct_frmts$param == ".default",]
-#'     )
-#'   }
-#'
-#'   for(frmt_idx in seq_len(nrow(struct_frmts))){
-#'
-#'     frmt_to_apply <- struct_frmts$frmt_to_apply[[frmt_idx]]
-#'     param_out_loc <- struct_frmts$param[[frmt_idx]]
-#'
-#'     .data_sub <- apply_frmt(
-#'         frmt_def = frmt_to_apply,
-#'         .data = .data_sub,
-#'         values = values,
-#'         group = group,
-#'         label = label,
-#'         column = column,
-#'         param = param,
-#'         param_val = param_out_loc,
-#'         frmt_values = frmt_values,
-#'         applied_frmt_values = applied_frmt_values
-#'       )
-#'   }
-#'
-#'   .data <- bind_rows(
-#'     .data[!struct_locs,],
-#'     .data_sub
-#'   )
-#'
-#'   .data
-#' }
+NULL
 
 
 #' Apply formatting
 #'
 #' @param .data data, but only what is getting changed
 #' @param frmt_def formatting to be applied
-#' @param param param symbol should only be one
-#' @param column column symbol should only be one
 #' @param values values symbol should only be one
+#' @param ... additional arguments for methods
 #'
-#' @return
 #' @noRd
 #' @importFrom stringr str_count str_trim str_dup str_c str_remove
 #' @importFrom dplyr if_else case_when tibble
-#' @importFrom purrr map_lgl
 apply_frmt <- function(frmt_def, .data, values, ...){
   UseMethod("apply_frmt", frmt_def)
 }
@@ -149,9 +28,9 @@ apply_frmt <- function(frmt_def, .data, values, ...){
 #'
 #' @return
 #' @noRd
-#' @importFrom stringr str_count str_trim str_dup str_c str_remove
-#' @importFrom dplyr if_else case_when tibble
-#' @importFrom purrr map_lgl
+#' @importFrom stringr str_count str_trim str_dup str_c str_remove str_extract
+#' @importFrom dplyr case_when tibble
+#' @importFrom purrr pluck
 apply_frmt.frmt <- function( frmt_def, .data, values, ...){
 
   vals <- .data %>%
@@ -178,12 +57,8 @@ apply_frmt.frmt <- function( frmt_def, .data, values, ...){
     act_pre_dec = rounded_vals %>%
       str_remove("\\..*$") %>%
       str_count("."),
-    # space_to_add = pre_dec - act_pre_dec
+    space_to_add = pmax(pre_dec - act_pre_dec,0) ## keep from being negative
   )
-
-  # if(any(fmt_options$space_to_add < 0)){
-  #   stop("Check format, there largest value is larger than expected")
-  # }
 
   if(!is.null(frmt_def$missing)){
     miss_val <- frmt_def$missing
@@ -191,7 +66,7 @@ apply_frmt.frmt <- function( frmt_def, .data, values, ...){
     miss_val <- NA_character_
   }
 
-  # fmt_vals <- str_c(str_dup(" ", fmt_options$space_to_add), fmt_options$rounded)
+  fmt_vals <- str_c(str_dup(" ", fmt_options$space_to_add), fmt_options$rounded)
 
   expr_start <- frmt_def$expression %>%
     str_extract("^[^X|^x]*(?=[X|x])")
@@ -202,7 +77,7 @@ apply_frmt.frmt <- function( frmt_def, .data, values, ...){
   # Combining the additional formatting
   fmt_val_output <- case_when(
     fmt_options$rounded == "NA" ~ miss_val,
-    TRUE ~ str_c(frmt_def$padding, expr_start, fmt_options$rounded, expr_end)
+    TRUE ~ str_c(frmt_def$padding, expr_start, fmt_vals, expr_end)
     )
 
   .data %>%
@@ -211,17 +86,21 @@ apply_frmt.frmt <- function( frmt_def, .data, values, ...){
     )
 
 }
+
+
+#' Apply frmt_combine information to data
 #'
+#' @param .data data, but only what is getting changed
+#' @param fmt_combine
+#' @param param
+#' @param values
 #'
-#' #' Apply frmt_combine information to data
-#' #'
-#' #' @param .data data, but only what is getting changed
-#' #' @param fmt_combine
-#' #' @param param
-#' #' @param values
-#' #'
-#' #' @return rounded and formatted df
-#' #' @noRd
+#' @return rounded and formatted df
+#' @importFrom stringr str_extract_all str_count str_trim str_dup str_c str_remove str_glue
+#' @importFrom dplyr case_when tibble filter pull left_join
+#' @importFrom tidyr pivot_wider
+#' @importFrom purrr map_dfr map_chr
+#' @noRd
 apply_frmt.frmt_combine <- function(frmt_def, .data, values, param, column, label, group, ...){
 
   fmt_param_vals <- frmt_def$expression %>%
@@ -233,6 +112,7 @@ apply_frmt.frmt_combine <- function(frmt_def, .data, values, param, column, labe
     stop("The values in the expression don't match the names of the given formats ")
   }
 
+  ## format params as needed
   .tmp_data <- map_dfr(fmt_param_vals, function(var){
     fmt_to_apply <- frmt_def$fmt_ls[[var]]
     .data %>%
@@ -274,10 +154,12 @@ apply_frmt.frmt_combine <- function(frmt_def, .data, values, param, column, labe
     frmt_def$missing <- ""
   }
 
+
+  ## if both params are missing, then drop in frmt definition missing value
+  ## otherwise concat the params
   .tmp_data_fmted <- .tmp_data_wide %>%
     mutate(
       !!values := case_when(
-        ## if both params are missing, then drop in frmt definition missing value
         .is_all_missing ~ frmt_def$missing,
         TRUE ~ str_glue(frmt_def$expression) %>% as.character()
         )
@@ -295,60 +177,9 @@ apply_frmt.frmt_combine <- function(frmt_def, .data, values, param, column, labe
 
 }
 
+#' @importFrom rlang parse_expr eval_bare
 all_missing <- function(cols, .data){
   paste0("is.na(.data$",cols,")", collapse = " & ") %>%
     parse_expr() %>%
     eval_bare(env = environment())
 }
-
-#'
-#' #' Test of the frmt of the data
-#' #'
-#' #' @param frmt_to_apply frmt to apply
-#' #' @param data data to test against
-#' #' @param group list of the group parameters
-#' #' @param label label symbol should only be one
-#' #' @param param param symbol should only be one
-#' #'
-#' #' @return vector of the rows which this format could be applied to
-#' #' @noRd
-#' idx_frmt_group_lab_rows <- function(struct_to_apply, .data, group, label, param){
-#'   group_locs <- eval_locs(group, .data, struct_to_apply$group[[1]])
-#'   label_locs <- eval_locs(label, .data, struct_to_apply$label)
-#'   group_locs & label_locs
-#' }
-#'
-#' eval_locs <- function(cols, .data, values){
-#'   UseMethod("eval_locs", cols)
-#' }
-#'
-#' eval_locs.quosure <- function(cols, .data, values){
-#'
-#'   if(".default" %in% values){
-#'     rep(TRUE, nrow(.data))
-#'   }else{
-#'     .data %>%
-#'       mutate(
-#'         .in_values = (!!cols) %in% values
-#'       ) %>%
-#'       pluck(".in_values")
-#'   }
-#'
-#' }
-#'
-#' eval_locs.quosures <- function(cols, .data, values){
-#'
-#'   if(!is.list(values)){
-#'     cols <- cols[[1]]
-#'     eval_locs(cols, .data, values)
-#'
-#'   }else{
-#'     stopifnot(all(names(values) %in% sapply(cols, as_label)))
-#'     .in_values_locs <- lapply(names(values),function(new_col){
-#'       new_quo <- quo(!!sym(new_col))
-#'       eval_locs(new_quo, .data, values[[new_col]])
-#'     })
-#'     Reduce(`&`, are_locs)
-#'   }
-#' }
-#'
