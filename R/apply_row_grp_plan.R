f03437199d262e14207e9ac91debcc5e2f75bb02
Becca Krouse <becca.z.krouse@gsk.com>
2022-05-20 14:47:56 -0400

    make row_grp_plan play nice with summary rows

--- R/apply_row_grp_plan.R
+++ R/apply_row_grp_plan.R
@@ -173,6 +173,11 @@ fill_post_space <- function(post_space, width){
 #'
 #' @return dataset with the group columns combines
 #' @noRd
+#' @importFrom dplyr group_by group_split mutate select distinct bind_rows across last
+#' @importFrom tidyr replace_na
+#' @importFrom stringr str_trim
+#' @importFrom purrr map_dfr
+#' @importFrom tidyselect vars_select_helpers
 combine_group_cols <- function(.data, group, label, element_row_grp_loc = NULL){
   top_grouping <- group #used for spliting in case of spanning label
 
@@ -186,21 +191,34 @@ combine_group_cols <- function(.data, group, label, element_row_grp_loc = NULL){
   }
 
   while(length(group) > 0 & !is.null(label)){
+
     split_dat <- .data %>%
       group_by(!!!top_grouping) %>%
       group_split()
 
     .data<- split_dat %>%
       map_dfr(function(lone_dat){
-        new_row <- lone_dat %>%
-          select(!!!top_grouping, !!label) %>%
-          mutate(!!label := !!last(group)) %>%
-          distinct()
 
-        lone_dat %>%
-          mutate(!!label := str_c(indent, !!label)) %>%
+        lone_dat_summ <- lone_dat %>%
+          mutate(..tlang_summary_row = str_trim(!!label, side = "left") == str_trim(!!last(group), side = "left"))
+
+        if (any(lone_dat_summ$..tlang_summary_row)==FALSE){
+          new_row <- lone_dat %>%
+            select(!!!top_grouping, !!label) %>%
+            mutate(!!label := !!last(group)) %>%
+            distinct()
+        } else {
+          new_row <- tibble()
+        }
+
+        lone_dat_summ %>%
+          # only indent if not a summary row
+          mutate(!!label := ifelse(.data$..tlang_summary_row==TRUE,
+                                   !!label,
+                                   str_c(indent, !!label))) %>%
+          select(-.data$..tlang_summary_row) %>%
           bind_rows(new_row, .) %>%
-          mutate(across(-c(!!!group), ~replace_na(., "")))
+          mutate(across(c(vars_select_helpers$where(is.character), -c(!!!group)), ~replace_na(., "")))
       })
     group = group[-length(group)]
     top_grouping = top_grouping[-length(top_grouping)]
