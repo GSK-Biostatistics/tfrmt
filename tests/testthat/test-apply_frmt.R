a168697dd671f208032ac057dd1a420b424117cc
statasaurus <55274484+statasaurus@users.noreply.github.com>
2022-06-30 09:02:49 +0100

    Adding tibble:: to all tribble references to pass tests

--- tests/testthat/test-apply_frmt.R
+++ tests/testthat/test-apply_frmt.R
@@ -413,7 +413,7 @@ test_that("applying frmt_when", {
     group = vars(group)
   )
 
-  man_df <- tribble(
+  man_df <- tibble::tribble(
     ~group, ~lab,        ~col, ~y,    ~x,
     "group", "lab 1", "col",   "A",     "1234.6",
     "group", "lab 2", "col",   "A",     "2345.7",
@@ -449,7 +449,7 @@ test_that("applying frmt_when", {
     group = vars(group)
   )
 
-  man_df_combo <- tribble(
+  man_df_combo <- tibble::tribble(
     ~group, ~lab,   ~col,   ~y,     ~x,
     "group", "lab 1", "col",  "A",     "1234.6 Undetectable",
     "group", "lab 2", "col",  "A",     "2345.7 Undetectable",
