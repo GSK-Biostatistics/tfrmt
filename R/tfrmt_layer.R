c5ceec74a17404e0bffe42602f39d22ba453cdee
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-07-01 06:52:52 -0700

    Add importFroms to update_group

--- R/tfrmt_layer.R
+++ R/tfrmt_layer.R
@@ -130,6 +130,8 @@ layer_tfrmt_arg.body_plan <- function(x, y, ...,  join_body_plans = TRUE){
 #' @return
 #' A `tfrmt` with the `group` variables updated in all places
 #'
+#' @importFrom rlang as_label is_empty
+#'
 #' @export
 #'
 #' @examples
