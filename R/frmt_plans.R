624a29503374af64ffd64b08a906a9b9d4f0b0cb
statasaurus <55274484+statasaurus@users.noreply.github.com>
2022-07-04 14:59:27 +0100

    Undo push into main

--- R/frmt_plans.R
+++ R/frmt_plans.R
@@ -162,6 +162,7 @@ frmt <- function(expression, missing = NULL, scientific = NULL, ...){
 #' @export
 #' @rdname frmt
 frmt_combine <- function(expression, ..., missing = NULL){
+
   everything_but_curly <- "(?<=\\{)([^}]*)(?=\\})"
 
   n_vars <- str_count(expression, everything_but_curly)
@@ -175,19 +176,8 @@ frmt_combine <- function(expression, ..., missing = NULL){
   }
 
   names(fmt_ls) <- vars_to_fmt
-  # Adding ` to expression if not there and there is a space/symbol
-  replace_val <- case_when(
-    str_detect(vars_to_fmt, "^[:alnum:]*$") ~ vars_to_fmt,
-    !str_detect(vars_to_fmt, "^[:alnum:]*$") & !str_detect(vars_to_fmt, "`") ~ paste0("`", vars_to_fmt, "`"),
-    TRUE ~ vars_to_fmt)
-
-  exp_new <- expression
-  for(i in 1:length(replace_val)){
-    exp_new <- stringr::str_replace(exp_new, vars_to_fmt[i], replace_val[i])
-  }
-
   structure(
-    list(expression = exp_new, fmt_ls = fmt_ls, missing = missing),
+    list(expression = expression, fmt_ls = fmt_ls, missing = missing),
     class = c("frmt_combine","frmt")
   )
 }
