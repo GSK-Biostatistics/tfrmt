d32245f22a56b37f34cf031cef5f125a03b2c2ea
karima0602 <72526731+karima0602@users.noreply.github.com>
2022-07-05 15:29:26 +0100

    refer to previous commit on this branch for changes #185

--- data-raw/create_data_demog.R
+++ data-raw/create_data_demog.R
@@ -231,3 +231,6 @@ create_tbl_demog_data <- function(){
 
 data_demog <- create_tbl_data_ae()
 usethis::use_data(data_demog, overwrite = TRUE)
+
+# Temporary Note: data_demog currently differs from demog_data
+
