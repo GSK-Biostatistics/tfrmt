0a163c235ee435a835db67b73945172e10f1e5c2
Shannon Haughton <shannon.l.haughton@gsk.com>
2022-07-19 11:11:13 +0100

    adding more tests

--- tests/testthat/test_print_to_ggplot.R
+++ tests/testthat/test_print_to_ggplot.R
@@ -129,3 +129,91 @@ test_that("tfrmt is as expected",{
 })
 
 
+test_that("column type has been preserved",{
+  # make discrete mock data
+  df <- structure(
+    list(
+      City = structure(
+        c(2L,
+          3L, 1L),
+        .Label = c("Minneapolis", "Phoenix",
+                   "Raleigh"),
+        class = "factor"
+      ),
+      January = c(52.1,
+                  40.5, 12.2),
+      February = c(55.1, 42.2, 16.5),
+      March = c(59.7, 49.2, 28.3),
+      April = c(67.7,
+                59.5, 45.1),
+      May = c(76.3, 67.4, 57.1),
+      June = c(84.6, 74.4, 66.9),
+      July = c(91.2,
+               77.5, 71.9),
+      August = c(89.1, 76.5,
+                 70.2),
+      September = c(83.8, 70.6, 60),
+      October = c(72.2, 60.2, 50),
+      November = c(59.8,
+                   50, 32.4),
+      December = c(52.5, 41.2,
+                   18.6)
+    ),
+    .Names = c(
+      "City",
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ),
+    class = "data.frame",
+    row.names = c(NA,-3L)
+  )
+
+  dfm <- df %>%
+    pivot_longer(January:December) %>%
+    rename("month" = name) %>%
+    mutate(month=substr(month,1,3))
+
+
+  table_data<- dfm %>%
+    mutate(param="n") %>%
+    rename("label"=City,"column"=month)
+
+  x2<-tfrmt(
+    # specify columns in the data
+    label = label ,
+    column = column,
+    param = param,
+    values = value) %>%
+    print_to_ggplot(table_data)
+
+  # continuous mock data
+  risk<-tibble(time=c(rep(c(0,1000,2000,3000),3)),
+               label=c(rep("Obs",4),rep("Lev",4),rep("Lev+5FU",4)),
+               value=c(630,372,256,11,620,360,266,8,608,425,328,14),
+               param=rep("n",12))
+
+  p2<-tfrmt(
+    # specify columns in the data
+    label = label ,
+    column = time,
+    param = param,
+    values = value) %>%
+    print_to_ggplot(risk)
+
+  expect_equal(class(table_data$column),class(x2$data$column))
+  expect_equal(class(risk$time),class(p2$data$column))
+
+
+})
+
+
