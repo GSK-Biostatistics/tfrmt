23b46e1dbba8c371f67abac5261861882bbb6838
Ellis Hughes <ellis.h.hughes@gsk.com>
2022-07-21 09:40:40 -0700

    allow wrapping in table body too

--- R/print_to_gt.R
+++ R/print_to_gt.R
@@ -190,7 +190,7 @@ cleaned_data_to_gt <- function(.data, tfrmt){
     format_gt_column_labels(.data) %>%
     apply_gt_col_style_plan_widths(tfrmt$col_style_plan) %>%
     tab_style(
-      style = cell_text(whitespace = "pre", align = align),
+      style = cell_text(whitespace = "pre-wrap", align = align),
       locations = cells_body(columns = everything())
     )
 
