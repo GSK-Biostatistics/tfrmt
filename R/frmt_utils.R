19313fa9c6dc96cc5533fa7f9d6750910ce8047b
karima0602 <72526731+karima0602@users.noreply.github.com>
2022-07-21 16:59:50 +0100

    edited format.frmt_when

--- R/frmt_utils.R
+++ R/frmt_utils.R
@@ -88,11 +88,11 @@ format.frmt_when <- function(x, ...){
   lhs <- map_chr(x, f_lhs)
   rhs <- map(x, f_rhs) %>% map_chr(format)
 
-  frmt_str <- cat(
+  frmt_str <- paste(
     "< frmt_when | ", "\n ",
     paste0(map2_chr(lhs, rhs, paste, sep = " ~ "),
     collapse = "\n  "),
-    "\n` >"
+    "\n >"
     )
   frmt_str
 }
