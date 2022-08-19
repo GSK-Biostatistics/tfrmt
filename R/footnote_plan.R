1b124e8624326923f629930a810478191c3b75a8
Shannon Haughton <shannon.l.haughton@gsk.com>
2022-08-19 11:12:42 +0100

    initial set up of footnote plan and structure

---
+++ R/footnote_plan.R
@@ -0,0 +1,66 @@
+#' Footnote Plan
+#'
+#' @param ...  footnote structure objects separated by commas
+#' @param marks type of marks required for footnotes. available options are "numbers", "letters", "standard" and "extended".
+#' The default option is set to "numeric".
+#'
+#' @return footnote plan object
+#' @export
+#'
+#' @examples
+footnote_plan <- function(...,marks=c("numbers","letters","standard","extended")){
+  footnote_structure_list <- list(...)
+  marks = match.arg(marks)
+
+  structure(
+    list(struct_list=footnote_structure_list, marks=marks),
+    class = c("footnote_plan", "plan")
+  )
+}
+
+#' Footnote Structure
+#'
+#' @param footnote_text string with text for footnote
+#' @param column_val string or a named list of strings which represent the column to apply the footnote to
+#' @param group_val string or a named list of strings which represent the value of group to apply the footnote to
+#' @param label_val string which represents the value of label to apply the footnote to
+#' @param param_val string which represents the value of param to apply the footnote to
+#'
+#' @return footnote structure object
+#' @export
+#'
+#' @examples
+footnote_structure <- function(footnote_text, column_val = ".default", group_val = ".default", label_val = ".default", param_val = ".default"){
+
+  # warnings if elements arent named
+
+  if(is.list(column_val)){
+    column_val_names <- names(column_val)
+    if(is.null(column_val_names)){
+      stop("when column_val is a list, must be a named list")
+    }else if(any(column_val_names == "")){
+      stop("when column_val is a list, each entry must be named")
+    }
+  }
+
+  if(is.list(group_val)){
+    group_val_names <- names(group_val)
+    if(is.null(group_val_names)){
+      stop("when group_val is a list, must be a named list")
+    }else if(any(group_val_names == "")){
+      stop("when group_val is a list, each entry must be named")
+    }
+  }
+
+   structure(
+    list(
+      column_val = column_val,
+      group_val = group_val,
+      label_val = label_val,
+      param_val = param_val,
+      footnote_text = footnote_text),
+    class = c("footnote_structure","structure")
+  )
+
+
+}
