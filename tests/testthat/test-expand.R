1e4857ac004b8a9ecc67d428ecc79ed48358b0ca
Christina Fillmore <55274484+statasaurus@users.noreply.github.com>
2023-02-09 14:46:03 +0000

    remove extra test I didn't use

--- tests/testthat/test-expand.R
+++
@@ -1,8 +0,0 @@
-test_that("Repo example", {
-  foo <- tibble(x1 = c(letters[1:3]),
-         x2 = c(1:3)) %>%
-    expand(x1, x2)
-  expect_equal(foo,
-               tibble(x1 = rep(letters[1:3], each = 3),
-                      x2 = rep(1:3, times = 3)))
-})
