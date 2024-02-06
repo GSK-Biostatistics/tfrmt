f28dd1c5a35b1622983a988fac8c8314791115c7
karima662 <karimajahmad6@gmail.com>
2024-02-06 15:20:41 +0000

    testing check_plan error message (for input supplied to col_style_plan)

--- tests/testthat/test-tfrmt_checks.R
+++ tests/testthat/test-tfrmt_checks.R
@@ -40,3 +40,39 @@ test_that("Testing error messages for missing argument col_style_structure",{
     ),
     "Column element is missing from col_style_structure[.] Note: col here refers to the values within the column variable in your data, rather than the variable name itself")
 })
+
+
+
+test_that("Testing error message for invalid input to plan parameters, specifically for col_style_plan",{
+
+  expect_error(
+    # create tfrmt object
+    tfrmt_object <- tfrmt(
+      label = label,
+      column = column,
+      param = param,
+      value = value,
+      sorting_cols = c(ord),
+      col_plan = col_plan(-ord),
+      body_plan = body_plan(
+        frmt_structure(group_val = ".default", label_val = ".default",
+                       frmt("xx", missing = " ")),
+        frmt_structure(group_val = ".default", label_val = ".default",
+                       frmt_combine("{mean} ({sd})",
+                                    mean = frmt("xx.x"),
+                                    sd = frmt("xx.xx"),
+                                    missing=" ")),
+        frmt_structure(group_val = ".default", label_val = ".default",
+                       pval = frmt_when(">0.99" ~ ">0.99",
+                                        "<0.001" ~ "<0.001",
+                                        "<0.05" ~ frmt("x.xxx*"),
+                                        TRUE ~ frmt("x.xxx", missing = "")))
+      ),
+      # col_style_structure() supplied to col_style_plan param instead of col_style_plan() funciton
+      col_style_plan = col_style_structure(col = `p-value`, align = c("."), type = "char")
+    ),
+    "Invalid input supplied to the col_style_plan parameter. Please supply a col_style_plan()."
+  )
+})
+
+
