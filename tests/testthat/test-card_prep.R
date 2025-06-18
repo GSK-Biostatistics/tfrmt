b2126d5fe3f865a1dbce6626b8bd7e00b644c582
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-06-18 13:05:40 +0100

    unit tests for extract_grouping_variables()

---
+++ tests/testthat/test-card_prep.R
@@ -0,0 +1,163 @@
+test_that("extract_grouping_variables() works with a single grouping variable", {
+  adsl <- pharmaverseadam::adsl |>
+    dplyr::filter(
+      SAFFL == "Y"
+    ) |>
+    dplyr::select(
+      USUBJID,
+      ARM,
+      SEX,
+      AGE,
+      AGEGR1,
+      ETHNIC,
+      RACE
+    ) |>
+    dplyr::mutate(
+      SEX = factor(
+        SEX,
+        levels = c("F", "M")
+      ),
+      AGEGR1 = factor(
+        AGEGR1,
+        levels = c(
+          "<18",
+          "18-64",
+          ">64"
+        ),
+        labels = c(
+          "<=18 years",
+          "Between 18 and 65 years",
+          ">=65 years"
+        )
+      ),
+      ETHNIC = factor(
+        ETHNIC,
+        levels = c(
+          "HISPANIC OR LATINO",
+          "NOT HISPANIC OR LATINO",
+          "NOT REPORTED"
+        )
+      ),
+      ,
+      RACE = factor(
+        RACE,
+        levels = c(
+          "AMERICAN INDIAN OR ALASKA NATIVE",
+          "ASIAN",
+          "BLACK OR AFRICAN AMERICAN",
+          "NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER",
+          "WHITE"
+        )
+      )
+    )
+
+  ard <- ard_stack(
+    data = adsl,
+    .by = ARM,
+    ard_continuous(
+      variables = AGE,
+      statistic = ~ continuous_summary_fns(c("N", "mean", "sd", "min", "max"))
+    ),
+    ard_categorical(
+      variables = c(
+        AGEGR1,
+        SEX,
+        ETHNIC,
+        RACE
+      )
+    ),
+    .overall = TRUE,
+    .total_n = TRUE
+  )
+
+  expect_equal(
+    extract_grouping_variables(ard),
+    "ARM"
+  )
+})
+
+test_that("extract_grouping_variables() works", {
+  adsl <- pharmaverseadam::adsl |>
+    dplyr::filter(
+      SAFFL == "Y"
+    ) |>
+    # create dummy site for demonstration purposes
+    dplyr::mutate(
+      COUNTRY = dplyr::if_else(
+        SITEID == 718,
+        "UK",
+        COUNTRY
+      )
+    )
+
+  # create siteid counts by country and arm
+  ard1 <- ard_strata(
+    .data = adsl,
+    .by = COUNTRY,
+    .f = ~ ard_categorical(
+      .x,
+      by = ARM,
+      variable = SITEID,
+      statistic = list(
+        SITEID = c("n", "p")
+      ),
+      denominator = adsl
+    )
+  )
+
+  # create ard for siteid counts by country (Total column)
+  ard2 <- ard_strata(
+    .data = adsl,
+    .by = COUNTRY,
+    .f = ~ ard_categorical(
+      .x,
+      variable = SITEID,
+      statistic = list(
+        SITEID = c("n", "p")
+      ),
+      denominator = adsl
+    )
+  ) |>
+    rename(
+      "group2" = group1,
+      "group2_level" = group1_level
+    ) |>
+    mutate(
+      group1 = "ARM",
+      group1_level = list("Total")
+    )
+
+  # counts by country
+  ard3 <- ard_stack(
+    data = adsl,
+    .by = ARM,
+    .overall = TRUE,
+    ard_categorical(
+      variables = COUNTRY,
+      statistic = everything() ~ c("n", "p")
+    )
+  ) |>
+    rename(
+      "group2" = variable,
+      "group2_level" = variable_level
+    ) |>
+    mutate(
+      variable = "SITEID",
+      variable_level = list("Subtotal")
+    )
+
+
+  final_ard <- bind_rows(
+    ard1,
+    ard2,
+    ard3,
+    ard_total_n(adsl)
+  ) |>
+    unlist_ard_columns()
+
+  expect_equal(
+    extract_grouping_variables(final_ard),
+    c("ARM", "COUNTRY")
+  )
+})
+
