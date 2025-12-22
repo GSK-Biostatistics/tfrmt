b4781bd4422f50907bf7bbe059485195111cf0f5
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2025-12-22 17:05:17 +0000

    win mem address

--- tests/testthat/helper-strip_env_label.R
+++ tests/testthat/helper-strip_env_label.R
@@ -3,8 +3,8 @@
 strip_env_label <- function(x) {
   stringr::str_replace_all(
     x,
-    # memory addresses have between 9 (macOS), 12(linux) or 15(windows) chars
-    pattern = "(0[xX]){1}[A-Fa-f0-9]{9,12}",
+    # memory addresses have 9 (macOS), 12(linux) or 16(windows) characters
+    pattern = "(0[xX]){1}[A-Fa-f0-9]{9,16}",
     replacement = "<env-address>"
   )
 }
