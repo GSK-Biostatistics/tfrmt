721d91c48604c03e92b9e997d83c7fdc476abbc0
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-07-08 14:58:08 +0100

    undo

--- R/apply_row_grp_plan.R
+++ R/apply_row_grp_plan.R
@@ -270,15 +270,15 @@ combine_group_cols <- function(.data, group, label, element_row_grp_loc = NULL){
     mutate(across(!!label, ~as.character(.x)))
 
   if(is.null(element_row_grp_loc)){
-    indent <- "  "
-  } else if(element_row_grp_loc$location == "spanning" & length(group) > 0){
+    indent = "  "
+  } else if(element_row_grp_loc$location=="spanning" & length(group) > 0){
     group = group[-1]
     indent = element_row_grp_loc$indent
   } else {
     indent = element_row_grp_loc$indent
   }
 
-  while (length(group) > 0 & !is.null(label)) {
+  while (length(group) > 0 & !is.null(label)){
     split_dat <- .data %>%
       group_by(run_id = dplyr::consecutive_id(!!!top_grouping)) %>%
       group_split() %>%
