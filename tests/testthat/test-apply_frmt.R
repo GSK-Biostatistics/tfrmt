d3f8c5c440c3e96db1bd3de20c2bc6a95c903751
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-07-09 09:28:47 +0100

    undo

--- tests/testthat/test-apply_frmt.R
+++ tests/testthat/test-apply_frmt.R
@@ -487,7 +487,7 @@ test_that("applying frmt_when", {
   )
 
   man_df_combo <- tibble::tribble(
-    ~group,   ~lab,   ~col,   ~y,     ~x,
+    ~group,     ~lab,   ~col,   ~y,     ~x,
     "group", "lab 1", "col",  "A",     "1234.6 Undetectable",
     "group", "lab 2", "col",  "A",     "2345.7 Undetectable",
     "group", "lab 3", "col",  "A",     "3456.8 (3.5%)",
@@ -495,6 +495,8 @@ test_that("applying frmt_when", {
     "group", "lab 5", "col",  "A",     "5678.9 (5.7%)",
   )
   expect_equal(sample_df_frmted, man_df_combo)
+
+
 })
 
 
