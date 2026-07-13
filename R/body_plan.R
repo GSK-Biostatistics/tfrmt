9ce062d08689cfdc95c7b74c183d8d1ff0fe14ee
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-07-13 15:07:23 +0100

    `R/b_` files formatting

--- R/body_plan.R
+++ R/body_plan.R
@@ -29,20 +29,23 @@
 #'
 #' @export
 #'
-body_plan <- function(...){
+body_plan <- function(...) {
+    frmt_structure_list <- list(...)
 
-  frmt_structure_list <- list(...)
-
-  for(struct_idx in seq_along(frmt_structure_list)){
-    if(!is_frmt_structure(frmt_structure_list[[struct_idx]])){
-      stop(paste0("Entry number ",struct_idx," is not an object of class `frmt_structure`."))
+    for (struct_idx in seq_along(frmt_structure_list)) {
+        if (!is_frmt_structure(frmt_structure_list[[struct_idx]])) {
+            stop(paste0(
+                "Entry number ",
+                struct_idx,
+                " is not an object of class `frmt_structure`."
+            ))
+        }
     }
-  }
 
-  structure(
-    frmt_structure_list,
-    class = c("body_plan", "frmt_table")
-  )
+    structure(
+        frmt_structure_list,
+        class = c("body_plan", "frmt_table")
+    )
 }
 
 
@@ -60,71 +63,109 @@ body_plan <- function(...){
 #' @importFrom dplyr mutate group_by filter group_split select across
 #' @importFrom tidyr unnest
 #' @importFrom rlang as_name quo_is_missing
-body_plan_builder <- function(data, group, label, param_defaults, missing = NULL){
+body_plan_builder <- function(
+    data,
+    group,
+    label,
+    param_defaults,
+    missing = NULL
+) {
+    # prep params for frmt functions
+    param_tbl <- seq_along(param_defaults) %>%
+        map_dfr(
+            ~ tibble(
+                param_display = names(param_defaults)[.x],
+                sigdig = list(param_defaults[[.x]] + data$sigdig[[1]]),
+                pos = .x
+            )
+        ) %>%
+        mutate(
+            contains_glue = str_detect(.data$param_display, "\\{.*\\}"), # is this to be a frmt_combine
+            param = map2(
+                .data$param_display,
+                .data$contains_glue,
+                ~ if (.y == TRUE) {
+                    str_extract_all(.x, "(?<=\\{)[^\\}]+(?=\\})") %>% unlist
+                } else {
+                    .x
+                }
+            ),
+            single_glue_to_frmt = pmap_chr(
+                list(.data$contains_glue, .data$param, .data$param_display),
+                function(a, b, c) {
+                    if (a == TRUE & length(b) == 1) c else NA_character_
+                }
+            )
+        ) %>%
+        unnest(
+            tidyselect::everything()
+        ) %>%
+        mutate(
+            frmt_string = map2_chr(
+                .data$sigdig,
+                .data$single_glue_to_frmt,
+                sigdig_frmt_string
+            )
+        )
 
-  # prep params for frmt functions
-  param_tbl <- seq_along(param_defaults) %>%
-    map_dfr(~tibble(param_display = names(param_defaults)[.x],
-                    sigdig = list(param_defaults[[.x]] + data$sigdig[[1]]),
-                    pos = .x)) %>%
-    mutate(contains_glue = str_detect(.data$param_display, "\\{.*\\}"),  # is this to be a frmt_combine
-           param = map2(.data$param_display, .data$contains_glue, ~ if(.y==TRUE){
-             str_extract_all(.x, "(?<=\\{)[^\\}]+(?=\\})") %>% unlist
-           } else {.x}),
-           single_glue_to_frmt = pmap_chr(list(.data$contains_glue, .data$param, .data$param_display), function(a,b,c){
-             if(a==TRUE & length(b) == 1) c else NA_character_
-           } )) %>%
-    unnest(
-      tidyselect::everything()
-    ) %>%
-    mutate(frmt_string = map2_chr(.data$sigdig, .data$single_glue_to_frmt, sigdig_frmt_string))
+    frmt_vec <- param_tbl %>%
+        group_by(.data$pos) %>%
+        group_split() %>%
+        map(function(x) {
+            if (sum(x$contains_glue) > 1) {
+                frmt_combine_builder(
+                    x$param_display[[1]],
+                    x$param,
+                    x$frmt_string,
+                    missing
+                )
+            } else {
+                frmt_builder(x$param, x$frmt_string, missing)
+            }
+        })
 
-  frmt_vec <- param_tbl %>%
-    group_by(.data$pos) %>%
-    group_split() %>%
-    map(function(x){
-      if(sum(x$contains_glue)>1){
-        frmt_combine_builder(x$param_display[[1]], x$param, x$frmt_string, missing)
-      } else{
-        frmt_builder(x$param, x$frmt_string, missing)
-      }
-    })
+    frmt_vec <- do.call(c, frmt_vec)
 
-  frmt_vec <-do.call(c, frmt_vec)
-
-  # group/label names from tfrmt
-  grp_names <- if (length(group)==0) character(0) else group %>% map_chr(as_name)
-  lbl_names <- if(quo_is_missing(label)) character(0) else as_name(label)
-
-  # sigdig value
-  sigdig <- data$sigdig[[1]]
-
-  which_grp <- grp_names[grp_names %in% names(data)]
-  which_lbl <- lbl_names[lbl_names %in% names(data)]
-
-  if(length(which_grp)>0){
-    group_val <- data[,which_grp] %>%
-      as.list() %>%
-      map(unique)
-
-    if (length(grp_names)>length(group_val)){
-      group_val_to_add <- grp_names[!grp_names %in% names(group_val)]
-      group_list_to_add <- rep(".default", length(group_val_to_add)) %>%
-        as.list() %>%
-        setNames(group_val_to_add)
-      group_val <- c(group_val, group_list_to_add)[grp_names]
+    # group/label names from tfrmt
+    grp_names <- if (length(group) == 0) {
+        character(0)
+    } else {
+        group %>% map_chr(as_name)
     }
-  } else {
-    group_val <- ".default"
-  }
+    lbl_names <- if (quo_is_missing(label)) character(0) else as_name(label)
 
-  if(length(which_lbl)>0){
-    label_val <- data[,which_lbl, drop = TRUE] %>% unique()
-    label_val <- if(any(label_val==".default")){".default"} else {label_val}
-  } else {
-    label_val <- ".default"
-  }
+    # sigdig value
+    sigdig <- data$sigdig[[1]]
 
-  frmt_structure_builder(group_val, label_val, frmt_vec)
+    which_grp <- grp_names[grp_names %in% names(data)]
+    which_lbl <- lbl_names[lbl_names %in% names(data)]
 
+    if (length(which_grp) > 0) {
+        group_val <- data[, which_grp] %>%
+            as.list() %>%
+            map(unique)
+
+        if (length(grp_names) > length(group_val)) {
+            group_val_to_add <- grp_names[!grp_names %in% names(group_val)]
+            group_list_to_add <- rep(".default", length(group_val_to_add)) %>%
+                as.list() %>%
+                setNames(group_val_to_add)
+            group_val <- c(group_val, group_list_to_add)[grp_names]
+        }
+    } else {
+        group_val <- ".default"
+    }
+
+    if (length(which_lbl) > 0) {
+        label_val <- data[, which_lbl, drop = TRUE] %>% unique()
+        label_val <- if (any(label_val == ".default")) {
+            ".default"
+        } else {
+            label_val
+        }
+    } else {
+        label_val <- ".default"
+    }
+
+    frmt_structure_builder(group_val, label_val, frmt_vec)
 }
