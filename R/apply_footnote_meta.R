0d648daad5164916d142b8c85e6ae841c6192689
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-10 13:47:35 +0300

    namespaced `inner_join()` calls

--- R/apply_footnote_meta.R
+++ R/apply_footnote_meta.R
@@ -108,7 +108,7 @@ get_col_loc <- function(footnote_structure, .data, col_plan_vars, columns) {
                 map_chr(as_label),
             col_str
         ) %>%
-            inner_join(loc_col_df, by = col_val_nm)
+            dplyr::inner_join(loc_col_df, by = col_val_nm)
 
         if (nrow(col_loc_df) == 0) {
             message_text <- c(
