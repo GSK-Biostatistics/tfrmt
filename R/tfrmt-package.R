0c25fcfba444f2f42a4164f6b611e78c695f913d
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-10 14:45:50 +0300

    do not import select

--- R/tfrmt-package.R
+++ R/tfrmt-package.R
@@ -8,7 +8,7 @@
 ## usethis namespace: start
 #' @importFrom dplyr lag last
 #' @importFrom dplyr left_join mutate n n_distinct na_if
-#' @importFrom dplyr select slice
+#' @importFrom dplyr slice
 #' @importFrom dplyr summarise ungroup
 #'
 #' @importFrom dplyr vars
