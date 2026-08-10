188f140ba4254435a25a5492224ee58290d7aa72
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-10 16:17:10 +0300

    namespaced ggplot2 calls

--- R/tfrmt-package.R
+++ R/tfrmt-package.R
@@ -15,10 +15,6 @@
 #'
 #' @importFrom forcats fct_inorder
 #'
-#' @importFrom ggplot2 aes element_blank element_text geom_text ggplot margin
-#' @importFrom ggplot2 scale_x_continuous scale_x_discrete scale_y_discrete
-#' @importFrom ggplot2 theme theme_void unit xlab ylab
-#'
 #' @importFrom glue glue glue_collapse
 #'
 #' @importFrom gt cells_body cell_borders cells_column_labels cells_column_spanners cells_row_groups cells_stub cell_text cols_hide cols_label default_fonts gt gt_group md opt_footnote_marks opt_table_font px sub_missing tab_footnote tab_header tab_options tab_source_note tab_spanner tab_stubhead tab_style text_transform
--- tests/testthat/test_print_to_ggplot.R
+++ tests/testthat/test_print_to_ggplot.R
@@ -60,7 +60,10 @@ test_that("inputs are as expected", {
         )
     )
 
-    expect_error(print_to_ggplot(tfrmt_1, "test"), "Requires data")
+    expect_error(
+        print_to_ggplot(tfrmt_1, "test"),
+        "Requires data"
+    )
     expect_error(
         print_to_ggplot(tfrmt = "test", .data = test_data),
         "Requires a tfrmt object"
@@ -112,7 +115,10 @@ test_that("group columns are created correctly", {
         `..tfrmt_row_grp_lbl` = c(TRUE, rep(FALSE, 12))
     )
 
-    expect_identical(apply_grp_ggplot(test_data, tfrmt), expected_data)
+    expect_identical(
+        apply_grp_ggplot(test_data, tfrmt),
+        expected_data
+    )
 })
 
 test_that("tfrmt is as expected", {
