92bd9a2f5edcc91c85d28ba8b9a614455b48b2d0
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-13 12:33:00 +0300

    unit tests for `is_tfrmt()` and `check_tfrmt()`

---
+++ tests/testthat/test-tfrmt.R
@@ -0,0 +1,44 @@
+test_that("is_tfrmt", {
+    expect_true(
+        is_tfrmt(
+            structure(
+                "foo",
+                class = "tfrmt"
+            )
+        )
+    )
+
+    expect_false(is_tfrmt("foo"))
+})
+
+test_that("check_tfrmt", {
+    expect_no_error(
+        check_tfrmt(
+            structure(
+                "foo",
+                class = "tfrmt"
+            )
+        )
+    )
+
+    expect_no_error(
+        check_tfrmt(
+            NULL,
+            allow_null = TRUE
+        )
+    )
+
+    expect_error(
+        check_tfrmt(
+            "foo"
+        ),
+        '`"foo"` must be a tfrmt object, not the string "foo"'
+    )
+
+    expect_error(
+        check_tfrmt(
+            TRUE
+        ),
+        '`TRUE` must be a tfrmt object, not `TRUE`'
+    )
+})
