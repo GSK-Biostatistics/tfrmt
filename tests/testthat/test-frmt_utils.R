8729445447aa828d8514d6d5920f093c6afaa0c7
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-13 17:23:58 +0300

    tests for `check_frmt()` and `check_frmt_strict()`

--- tests/testthat/test-frmt_utils.R
+++ tests/testthat/test-frmt_utils.R
@@ -14,3 +14,71 @@ test_that("is_frmt", {
         )
     )
 })
+
+test_that("check_frmt", {
+    expect_no_error(
+        check_frmt(
+            structure(
+                "foo",
+                class = "frmt"
+            )
+        )
+    )
+
+    expect_no_error(check_frmt(NULL, allow_null = TRUE))
+
+    expect_error(
+        check_frmt("foo", ),
+        '`"foo"` must be a frmt object, not the string "foo".'
+    )
+})
+
+test_that("is_frmt_strict", {
+    expect_true(
+        is_frmt_strict(
+            structure(
+                rlang::expr(foo(bar)),
+                class = "frmt"
+            )
+        )
+    )
+
+    # the input must not have additional classes
+    expect_false(
+        is_frmt_strict(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt", "frmt_when")
+            )
+        )
+    )
+})
+
+test_that("check_frmt_strict", {
+    expect_no_error(
+        check_frmt_strict(
+            structure(
+                rlang::expr(foo(bar)),
+                class = "frmt"
+            )
+        )
+    )
+
+    expect_no_error(
+        check_frmt_strict(
+            NULL,
+            allow_null = TRUE
+        )
+    )
+
+    # the input must not have additional classes
+    expect_error(
+        check_frmt_strict(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt_when", "frmt")
+            )
+        ),
+        "must be a <frmt> object, not a <frmt_when> object."
+    )
+})
