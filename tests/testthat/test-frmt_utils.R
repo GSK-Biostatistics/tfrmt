2b4ef2e68800b0da060cfd0d4a135c237b07904a
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-13 17:29:20 +0300

    tests for `check_frmt_combine()`

--- tests/testthat/test-frmt_utils.R
+++ tests/testthat/test-frmt_utils.R
@@ -82,3 +82,56 @@ test_that("check_frmt_strict", {
         "must be a <frmt> object, not a <frmt_when> object."
     )
 })
+
+test_that("is_frmt_combine", {
+    expect_true(
+        is_frmt_combine(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt", "frmt_combine")
+            )
+        )
+    )
+
+    expect_true(
+        is_frmt_combine(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt_combine")
+            )
+        )
+    )
+
+    expect_false(
+        is_frmt_combine(
+            "foo"
+        )
+    )
+})
+
+test_that("check_frmt_combine", {
+    expect_no_error(
+        check_frmt_combine(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt", "frmt_combine")
+            )
+        )
+    )
+
+    expect_no_error(
+        check_frmt_combine(
+            structure(
+                rlang::expr(foo(bar)),
+                class = c("frmt_combine")
+            )
+        )
+    )
+
+    expect_no_error(check_frmt_combine(NULL, allow_null = TRUE))
+
+    expect_error(
+        check_frmt_combine("foo"),
+        '`"foo"` must be a frmt combine object, not the string "foo".'
+    )
+})
