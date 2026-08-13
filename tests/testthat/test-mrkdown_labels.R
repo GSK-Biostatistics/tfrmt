13e74786aca7deb510bbd29da8093cd7d40e6006
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-13 18:12:41 +0300

    fix

--- tests/testthat/test-mrkdown_labels.R
+++ tests/testthat/test-mrkdown_labels.R
@@ -260,7 +260,7 @@ test_that("column spanners and labels are appropriately aligned", {
 
     # get tfrmt cols from spec
     chr_cols <- tfrmt_spec$column |>
-        purrr::map_chr(, rlang::as_name) |>
+        purrr::map_chr(rlang::as_name) |>
         rev()
 
     # combine spanner & lower labels and rename as per tfrmt spec
