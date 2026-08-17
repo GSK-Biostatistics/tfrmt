7e6f4a30c1dcaa18ccb1269be69c4f5338cb7bde
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-17 10:11:14 +0300

    export S3 methods

--- R/eval_tidyselect.R
+++ R/eval_tidyselect.R
@@ -5,6 +5,7 @@ eval_tidyselect_on_colvec <- function(x, column_vec) {
     UseMethod("eval_tidyselect_on_colvec", x)
 }
 
+#' @export
 eval_tidyselect_on_colvec.quosures <- function(x, column_vec) {
     names(column_vec) <- column_vec
     avli_x <- x %>%
@@ -17,7 +18,7 @@ eval_tidyselect_on_colvec.quosures <- function(x, column_vec) {
         )
     )
 }
-
+#' @export
 eval_tidyselect_on_colvec.quosure <- function(x, column_vec) {
     names(column_vec) <- column_vec
 
