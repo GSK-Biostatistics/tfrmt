528bc03378de785fe3e29cf47675bafbe03d7758
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2026-08-25 13:37:26 +0100

    added `check_element_block()` + tests

--- R/theme_element.R
+++ R/theme_element.R
@@ -97,7 +97,10 @@ is_element_row_grp_loc <- function(x) {
 #' )
 element_block <- function(post_space = c(NULL, " ", "-"), fill = TRUE) {
     structure(
-        list(post_space = post_space, fill = fill),
+        list(
+            post_space = post_space,
+            fill = fill
+        ),
         class = c("element_block", "element")
     )
 }
@@ -106,6 +109,30 @@ is_element_block <- function(x) {
     inherits(x, "element_block")
 }
 
+check_element_block <- function(
+    element_block,
+    arg = rlang::caller_arg(element_block),
+    call = rlang::caller_env(),
+    allow_null = FALSE
+) {
+    if (!missing(element_block)) {
+        if (is_element_block(element_block)) {
+            return(invisible(NULL))
+        }
+
+        if (allow_null && is.null(element_block)) {
+            return(invisible(NULL))
+        }
+    }
+
+    rlang::stop_input_type(
+        element_block,
+        "an element_block object",
+        allow_null = allow_null,
+        arg = arg,
+        call = call
+    )
+}
 
 element_stub <- function(
     collapse_ord = vars(),
